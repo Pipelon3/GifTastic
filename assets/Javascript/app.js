d89cdc44cd1701be1ac422e75bd49a73678a4142
Pipelon3 <Pipelon999@gmail.com>
2019-06-21 23:04:44 -0600

    working!

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,19 +1,4 @@
-// Stuff to make it work
-
-$("#games").on("click", function() {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + myGames + "&api_key=5KZDAjtUy2sVI1b6KIs3blyHtx4vUAK9&limit=10";
-    let myGames=["Legend of zelda","Call of Duty","Kingdom Hearts"]
-    
-})
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-})
-
-  // Pause Gifs on click
+// Pause Gifs on click
 
 $(".gif").on("click", function() {    
   var state = $(this).attr("data-state");
@@ -25,3 +10,49 @@ $(".gif").on("click", function() {
       $(this).attr("data-state", "still");
     }
 });
+
+//Stuff to make it work
+
+var games = ["The Legend of Zelda", "Bioshock", "Kingdom Hearts", "The Witcher"];
+    
+function displayGameInfo() {
+    var game = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5KZDAjtUy2sVI1b6KIs3blyHtx4vUAK9&q=" + game + "&limit=10&offset=0&rating=R&lang=en";
+
+    $.ajax({
+      url: queryURL,
+      contentType: "text/html",
+      method: "GET"
+      }).then(function(response) {
+        $("#game-view").empty();
+        for ( var i = 0; i < response.data.length; i ++){
+          var a = $("<iframe>");
+          a.addClass("game");
+          a.attr('src', response.data[i].embed_url);
+          a.attr('type', response.data[i].type);
+          $("#game-view").append(a);
+        }         
+      });            
+    }
+
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < games.length; i++) {
+        var a = $("<button>");
+        a.addClass("game-btn");
+        a.attr("data-name", games[i]);
+        a.text(games[i]);
+        $("#buttons-view").append(a);
+    }
+}
+
+$("#add-game").on("click", function(event) {
+    event.preventDefault();
+    var game = $("#game-input").val().trim();
+    games.push(game);
+    renderButtons();
+});
+
+$(document).on("click", ".game-btn", displayGameInfo);
+
+renderButtons();
